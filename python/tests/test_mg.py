afc1718603b2b7254ff6592b9f2a9cd591b5a0bb
Michael Goodale <michael.goodale@mail.mcgill.ca>
2025-04-18 11:03:58 +0200

    Added basic code plus tests plus gitignore

---
+++ python/tests/test_mg.py
@@ -0,0 +1,13 @@
+import pytest
+
+from python_mg._lib_name import Lexicon
+
+
+def test_lexicon():
+    x = Lexicon("a::b= a\nb::b")
+    assert [str(s) for s in x.generate_grammar("a")] == ["a b"]
+    parse = next(x.generate_grammar("a"))
+    assert (
+        parse.latex()
+        == "\\begin{forest}\n[{\\der{a}}\n\t[{\\plainlex{a}{\\cancel{b=} a}} ]\n\t[{\\plainlex{b}{\\cancel{b}}} ] ]\n\\end{forest}"
+    )
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,116 @@
+use std::fmt::Display;
+
+use anyhow::anyhow;
+use logprob::LogProb;
+use minimalist_grammar_parser::{lexicon::Lexicon, Generator, ParsingConfig, RulePool};
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyclass(name = "SyntacticStructure", str, eq, frozen)]
+#[derive(Debug)]
+struct PySyntacticStructure {
+    prob: LogProb<f64>,
+    string: Vec<String>,
+    rules: RulePool,
+    lex: Py<PyLexicon>,
+}
+
+impl PartialEq for PySyntacticStructure {
+    fn eq(&self, other: &Self) -> bool {
+        self.prob == other.prob && self.string == other.string && self.rules == other.rules
+    }
+}
+
+impl Display for PySyntacticStructure {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.string.join(" "))
+    }
+}
+
+#[pymethods]
+impl PySyntacticStructure {
+    fn log_prob(&self) -> f64 {
+        self.prob.into_inner()
+    }
+
+    fn prob(&self) -> f64 {
+        self.prob.into_inner().exp()
+    }
+
+    fn latex(&self) -> String {
+        let lex = self.lex.get();
+        self.rules.to_latex(&lex.0)
+    }
+}
+
+#[pyclass(name = "Lexicon", str, eq, frozen)]
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct PyLexicon(Lexicon<String, String>);
+
+impl Display for PyLexicon {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "MGLexicon{{\n{}\n}}", self.0)
+    }
+}
+
+#[pyclass]
+struct GrammarIterator(
+    Generator<Lexicon<String, String>, String, String>,
+    Py<PyLexicon>,
+);
+
+#[pymethods]
+impl GrammarIterator {
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    fn __next__(mut slf: PyRefMut<'_, Self>) -> Option<PySyntacticStructure> {
+        let py = slf.py();
+        slf.0
+            .next()
+            .map(|(prob, string, rules)| PySyntacticStructure {
+                prob,
+                string,
+                rules,
+                lex: slf.1.clone_ref(py),
+            })
+    }
+}
+
+#[pymethods]
+impl PyLexicon {
+    #[pyo3(signature = (category, min_log_prob=-128.0, move_prob=0.5, max_steps=64, n_beams=256))]
+    fn generate_grammar(
+        slf: PyRef<'_, Self>,
+        category: String,
+        min_log_prob: f64,
+        move_prob: f64,
+        max_steps: usize,
+        n_beams: usize,
+    ) -> PyResult<GrammarIterator> {
+        let config = ParsingConfig::new(
+            LogProb::new(min_log_prob).map_err(|x| anyhow!(x.to_string()))?,
+            LogProb::from_raw_prob(move_prob).map_err(|x| anyhow!(x.to_string()))?,
+            max_steps,
+            n_beams,
+        );
+        let py = slf.py();
+        Ok(GrammarIterator(
+            Generator::new(slf.0.clone(), category, &config)?,
+            slf.into_pyobject(py).unwrap().into(),
+        ))
+    }
+
+    #[new]
+    fn new(s: &str) -> PyResult<PyLexicon> {
+        Ok(PyLexicon(Lexicon::parse(s).unwrap().to_owned_values()))
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 #[pyo3(name = "_lib_name")]
 fn python_mg(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<PyLexicon>()?;
     Ok(())
 }
